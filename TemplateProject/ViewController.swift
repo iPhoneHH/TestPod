633bcb5113053589108d80f457037804c7d3b5e3
CasaTaloyum <casatwy@msn.com>
2018-10-13 18:12:45 +0800

    Initial Commit

---
+++ TemplateProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TemplateProject
+//
+//  Created by casa on 2018/10/13.
+//  Copyright © 2018 casa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
